f3d2f4b7efe6d346291da9e3484e74e5cf53c002
Surajit Pal <surajitx.pal@intel.com>
2020-05-20 16:19:17 -0700

    FAST fixed typo 1
    Signed-off-by: Surajit Pal <surajitx.pal@intel.com>

--- tests/utils/rmvDockerImgs.sh
+++ tests/utils/rmvDockerImgs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "Remove all EDGEX-FOUNDRY imgaes"
+echo "Remove all EDGEX-FOUNDRY images"
 $(docker images -a  | grep "edgexfoundry" | awk '{print $3}' | xargs docker rmi -f)
 
-echo "Remove all OTHERS imgaes"
+echo "Remove all OTHERS images"
 $(docker "rmi" "$(docker images -a -q)")
 
-echo "Remove all DANGLING imgaes"
+echo "Remove all DANGLING images"
 $(docker rmi $(docker images -f dangling=true -q))
