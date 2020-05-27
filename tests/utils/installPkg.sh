630d0a962603729e8fbdb7c80fedd6af8b3fa471
Surajit Pal <surajitx.pal@intel.com>
2020-05-27 16:04:37 -0700

    Jenkins debugging 15
    Signed-off-by: Surajit Pal <surajitx.pal@intel.com>

--- tests/utils/installPkg.sh
+++ tests/utils/installPkg.sh
@@ -13,10 +13,10 @@ PING1="$(ping -c 1 8.8.8.8)"
 
 if [[ $PING1 == *"unreachable"* ]]; then
     printDatedErrMsg "ERROR: No network connection found, exiting."
-#    exit 1
+    exit 1
 elif [[ $PING1 == *"100% packet loss"* ]]; then
     printDatedErrMsg "ERROR: No Internet connection found, exiting."
-#    exit 1
+    exit 1
 else
     printDatedOkMsg "Connectivity OK"
 fi
