17f56f034f6caa69c2ccffcce29ac6e4ac6c9717
Surajit Pal <surajitx.pal@intel.com>
2020-06-17 12:59:28 -0700

    Update the Script for Stop the unused containers and Remove dangling images
    Signed-off-by: Surajit Pal <surajitx.pal@intel.com>

--- tests/utils/rmvDockerImgs.sh
+++ tests/utils/rmvDockerImgs.sh
@@ -4,10 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 echo "Remove all EDGEX-FOUNDRY images"
-$(docker images -a  | grep "edgexfoundry" | awk '{print $3}' | xargs docker rmi -f)
+$(docker images -a | grep  -v "device-llrp-go" | grep "edgexfoundry" | awk '{print $3}' | xargs docker rmi -f)
 
-echo "Cleaning up unused resources"
-$(docker "rmi" "$(docker images -a -q)")
+echo "Remove unused containers resources then remove dangling images"
 
-echo "Cleaning all DANGLING images"
-$(docker rmi $(docker images -f dangling=true -q))
+$(docker rm -v $(docker ps -aqf status=exited))
+$(docker rmi $(docker images -qf dangling=true))
+$(docker volume rm $(docker volume ls -qf dangling=true))
