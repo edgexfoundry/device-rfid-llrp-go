58a2a993015e9a67635511414aaf5a527c53e756
Anthony Casagrande <anthony.j.casagrande@intel.com>
2021-05-17 15:06:26 -0700

    Auto config script updates and readability (#7)
    - Add error trap for failed commands (find, curl, ip, etc)
    - Improve readability (variables for ansi escape codes)
    - Fix comparison logic for curl output code
    - Remove link-local subnet from output
    
    Signed-off-by: Anthony Casagrande <anthony.j.casagrande@intel.com>

--- bin/auto-configure.sh
+++ bin/auto-configure.sh
@@ -8,34 +8,42 @@
 
 set -eu
 
+spacing=18; prev_line="\e[1A\e[$((spacing + 2))C"
+green="\e[32m"; red="\e[31m"; clear="\e[0m"; bold="\e[1m"; normal="\e[22;24m"
+
+trap 'err' ERR
+err() {
+    echo -e "${red}${bold}Failed!${clear}"
+    exit 1
+}
+
 CONSUL_URL=${CONSUL_URL:-http://localhost:8500}
 url="${CONSUL_URL}/v1/kv/edgex/devices/1.0/edgex-device-rfid-llrp/Driver/DiscoverySubnets"
 
-printf "\e[1m%18s\e[0m: ..." "Dependencies Check"
-if ! type -P curl > /dev/null; then
-    printf "\e[3D\e[1;31mFailed!\e[22;24m
-Please install \e[1mcurl\e[22;24m in order to use this script!\e[0m
-"
+### Dependencies Check
+printf "${bold}%${spacing}s${clear}: ...\n${red}" "Dependencies Check"
+if ! type -P curl >/dev/null; then
+    echo -e "${bold}${red}Failed!${normal} Please install ${bold}curl${normal} in order to use this script!${clear}"
     exit 1
 fi
-printf "\e[3D\e[32mSuccess\e[0m\n"
+echo -e "${prev_line}${green}Success${clear}"
 
-printf "\e[1m%18s\e[0m: ..." "Consul Check"
-code=$(curl -X GET -w "%{http_code}" -o /dev/null -s "${url}")
-
-if [ "${code}" -ne 200 ]; then
-    printf "\e[3D\e[1;31mFailed!\e[22;24m
-curl returned a status code of \e[1m%s\e[0m
-" "${code}" 1>&2
-    if [ "${code}" -eq 404 ]; then
-        printf -- "* Have you deployed the \e[1mdevice-rfid-llrp\e[0m service?\n"
+### Consul Check
+printf "${bold}%${spacing}s${clear}: ...\n${red}" "Consul Check"
+code=$(curl -X GET -w "%{http_code}" -o /dev/null -s "${url}" || echo $?)
+if [ $((code)) -ne 200 ]; then
+    echo -e "${red}${bold}Failed!${normal} curl returned a status code of '${bold}${code}'${normal}"
+    if [ $((code)) -eq 404 ]; then
+        echo -e "* Have you deployed the ${bold}device-rfid-llrp${normal} service?${clear}"
     else
-        printf -- "* Is Consul deployed and accessible?\n"
+        echo -e "* Is Consul deployed and accessible?${clear}"
     fi
     exit $((code))
 fi
-printf "\e[3D\e[32mSuccess\e[0m\n"
+echo -e "${prev_line}${green}Success${clear}"
 
+### Detect Interfaces
+printf "${bold}%${spacing}s${clear}: ...\n${red}" "Interfaces"
 # find all online non-virtual network interfaces, separated by `|` for regex matching. ie. (eno1|eno2|eno3|...)
 ifaces=$(
     find /sys/class/net -mindepth 1 -maxdepth 2 \
@@ -43,28 +51,35 @@ ifaces=$(
         -execdir grep -q 'up' "{}/operstate" \; \
         -printf '%f|'
 )
+if [ -z "${ifaces}" ]; then
+    echo "Error, no online physical network interfaces detected.${clear}"
+    exit 1
+fi
+echo -e "${prev_line}${clear}${ifaces//|/ }"
 
+### Detect Subnets
+printf "${bold}%${spacing}s${clear}: ...\n${red}" "Subnets"
 # print all ipv4 subnets, filter for just the ones associated with our physical interfaces
 # grab the unique ones and join them by commas
 subnets=$(
-    ip -4 -o route list scope link | \
-    sed -En "s/ dev (${ifaces::-1}).+//p" | \
-    sort -u | \
-    paste -sd, -
+    ip -4 -o route list scope link |
+        sed -En "s/ dev (${ifaces::-1}).+//p" |
+        grep -v "169.254.0.0/16" | # Remove link-local subnet
+        sort -u |
+        paste -sd, -
 )
 
-printf "\e[1m%18s\e[0m: %s\n" "Subnets" "${subnets}"
 if [ -z "${subnets}" ]; then
-    echo "Error, no subnets detected" 1>&2
+    echo -e "Error, no subnets detected.${clear}"
     exit 1
 fi
+echo -e "${prev_line}${clear}${subnets}"
 
-printf "\e[1m%18s\e[0m: ..." "Configure"
-code=$(curl -X PUT --data "${subnets}" -w "%{http_code}" -o /dev/null -s "${url}")
-if [ "${code}" -ne 200 ]; then
-    printf "\e[3D\e[1;31mFailed!\e[31m
-curl returned a status code of \e[1m%s\e[0m
-" "${code}"
+### Configure Consul
+printf "${bold}%${spacing}s${clear}: ...\n${red}" "Configure"
+code=$(curl -X PUT --data "${subnets}" -w "%{http_code}" -o /dev/null -s "${url}" || echo $?)
+if [ $((code)) -ne 200 ]; then
+    echo -e "${red}${bold}Failed!${normal} curl returned a status code of '${bold}${code}'${clear}"
     exit $((code))
 fi
-printf "\e[3D\e[32mSuccess\e[0m\n"
+echo -e "${prev_line}${green}Success${clear}"
