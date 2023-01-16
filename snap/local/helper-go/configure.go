3a36f0f9096bbb25862370b2ce371015e7cf52bc
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2023-01-16 10:21:12 +0000

    refactor(snap)!: Remove support for legacy env options BREAKING CHANGE: Drop the support for deprecated snap options starting with `env.`
    Signed-off-by: Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>

--- snap/local/helper-go/configure.go
+++ snap/local/helper-go/configure.go
@@ -1,5 +1,3 @@
-// -*- Mode: Go; indent-tabs-mode: t -*-
-
 /*
  * Copyright (C) 2021 Canonical Ltd
  *
@@ -19,73 +17,18 @@
 package main
 
 import (
-	hooks "github.com/canonical/edgex-snap-hooks/v2"
-	"github.com/canonical/edgex-snap-hooks/v2/log"
-	"github.com/canonical/edgex-snap-hooks/v2/options"
+	"github.com/canonical/edgex-snap-hooks/v3/log"
+	"github.com/canonical/edgex-snap-hooks/v3/options"
 )
 
-// ConfToEnv defines mappings from snap config keys to EdgeX environment variable
-// names that are used to override individual device-rfid-llrp's [Device]  configuration
-// values via a .env file read by the snap service wrapper.
-//
-// The syntax to set a configuration key is:
-//
-// env.<section>.<keyname>
-//
-var ConfToEnv = map[string]string{
-	// [Device]
-	"device.update-last-connected": "DEVICE_UPDATELASTCONNECTED",
-	"device.use-message-bus":       "DEVICE_USEMESSAGEBUS",
-
-	// [AppCustom]
-	// List of IPv4 subnets to perform LLRP discovery process on, in CIDR format (X.X.X.X/Y)
-	// separated by commas ex: "192.168.1.0/24,10.0.0.0/24"
-	"app-custom.discovery-subnets": "APPCUSTOM_DISCOVERYSUBNETS",
-
-	// Maximum simultaneous network probes
-	"app-custom.probe-async-limit": "APPCUSTOM_PROBEASYNCLIMIT",
-
-	// Maximum amount of seconds to wait for each IP probe before timing out.
-	// This will also be the minimum time the discovery process can take.
-	"app-custom.probe-timeout-seconds": "APPCUSTOM_PROBETIMEOUTSECONDS", // ProbeTimeoutSeconds = 2
-
-	// Port to scan for LLRP devices on
-	"app-custom.scan-port": "APPCUSTOM_SCANPORT",
-
-	// Maximum amount of seconds the discovery process is allowed to run before it will be cancelled.
-	// It is especially important to have this configured in the case of larger subnets such as /16 and /8
-	"app-custom.max-discover-duration-seconds": "APPCUSTOM_MAXDISCOVERDURATIONSECONDS",
-}
-
-// configure is called by the main function
 func configure() {
-
-	const service = "device-rfid-llrp"
-
 	log.SetComponentName("configure")
 
-	log.Info("Processing legacy env options")
-	envJSON, err := hooks.NewSnapCtl().Config(hooks.EnvConfig)
-	if err != nil {
-		log.Fatalf("Reading config 'env' failed: %v", err)
-	}
-	if envJSON != "" {
-		log.Debugf("envJSON: %s", envJSON)
-		err = hooks.HandleEdgeXConfig(service, envJSON, ConfToEnv)
-		if err != nil {
-			log.Fatalf("HandleEdgeXConfig failed: %v", err)
-		}
+	if err := options.ProcessConfig(app); err != nil {
+		log.Fatalf("Error processing config options: %v", err)
 	}
 
-	log.Info("Processing config options")
-	err = options.ProcessConfig(service)
-	if err != nil {
-		log.Fatalf("could not process config options: %v", err)
-	}
-
-	log.Info("Processing autostart options")
-	err = options.ProcessAutostart(service)
-	if err != nil {
-		log.Fatalf("could not process autostart options: %v", err)
+	if err := options.ProcessAutostart(app); err != nil {
+		log.Fatalf("Error processing autostart options: %v", err)
 	}
 }
